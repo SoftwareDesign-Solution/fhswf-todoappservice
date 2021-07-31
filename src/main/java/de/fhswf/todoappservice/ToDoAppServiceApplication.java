cda73843267ef781f005efc4afcd58cfa3111cff
softwaredesign-solution <mail@softwaredesign-solution.de>
2021-07-31 22:47:45 +0200

    - Projekt erstellt

---
+++ src/main/java/de/fhswf/todoappservice/ToDoAppServiceApplication.java
@@ -0,0 +1,13 @@
+package de.fhswf.todoappservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoAppServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoAppServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/fhswf/todoappservice/ToDoAppServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fhswf.todoappservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoAppServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
